9fb84478a7c9d2eac7d264161ca523ecbd2e29a8
akieliszczyk <38585186+akieliszczyk@users.noreply.github.com>
2020-08-04 09:05:24 +0200

    Create ProgramData.java
    Class contains getters and setters

---
+++ src/main/java/model/ProgramData.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ProgramData {
+
+    private Integer employee;
+    private Integer plc;
+
+    public Integer getEmployee() {
+        return employee;
+    }
+
+    public Integer getPlc() {
+        return plc;
+    }
+
+    public void setEmployee(Integer employee) {
+        this.employee = employee;
+    }
+
+    public void setPlc(Integer plc) {
+        this.plc = plc;
+    }
+}
