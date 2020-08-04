1942bcd9b1beafc346b24ade254be6f5da99d10f
akieliszczyk <38585186+akieliszczyk@users.noreply.github.com>
2020-08-04 09:07:43 +0200

    Create Execute.java

---
+++ src/test/java/Execute.java
@@ -0,0 +1,10 @@
+import model.Counting;
+
+public class Execute {
+
+    public static void main(String[] args) {
+
+        Counting counting = new Counting();
+        counting.countDivisors();
+    }
+}
